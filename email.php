84faa578ddd99b52a34dc42ea7195fbffe804738
Aiden Cohen <aidencohen31@gmail.com>
2019-03-02 05:20:33 +0000

    Added email and suggestion box implemented php

--- email.php
+++ email.php
@@ -1,12 +1,23 @@
 <html>
 
 <body>
-<?php>
-$msg = $_POST["suggs"];
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL ^ E_NOTICE);
+$msg = $_POST["sugg"];
 $name = $_POST["name"];
-mail("aidencohen31@gmail.com","website suggs" .= $_POST["name"] ,$msg);
+$email = $_POST["email"];
+$c = 'website suggs';
+$d = $c .= $name;
+$thanks = "Thank You for making my website better. I will definitely look forward to implementing".$msg."Thank You for taking your time to do this. Sincerely, Aiden Cohen.";
+if(mail("aidencohen31@gmail.com",$d ,$msg ))
+{
+echo "message sent";
+}
+mail($email, "Thank You", $thanks );
 ?>
 
+<a href = aidencohen.zapto.org/index.html> return to home </a>
 </body>
 
 
